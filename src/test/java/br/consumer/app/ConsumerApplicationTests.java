91c78842c43b65c8fb770136555d1daa691ec758
Eduardo Andrade Santos <eandrasa@everis.com>
2020-11-19 10:09:55 -0300

    consumidora de mensagem rabbitmq

---
+++ src/test/java/br/consumer/app/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.consumer.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
